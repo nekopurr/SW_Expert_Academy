744de19912da07cbc8e90899e4fea00326f5ff91
GoreNeko <85627591+GoreNeko@users.noreply.github.com>
2022-11-19 03:31:36 +0900

    11/19

--- Level3/14692_통나무자르기.py
+++ Level3/14692_통나무자르기.py
@@ -1,4 +1,4 @@
-
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=3&contestProbId=AYJW0g-qlO8DFASv&categoryId=AYJW0g-qlO8DFASv&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=3&pageSize=10&pageIndex=1
 
 T = int(input())
 for TC in range(1, T + 1):
